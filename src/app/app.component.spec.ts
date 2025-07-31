34aef6a44bc270ae339a03211901e127239ad2a9
m.dili <m.dili@reply.de>
2025-07-31 12:20:43 +0200

    Fixed tests;

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+
+// Create a simple mock component.
+// It is NOT standalone because the component using it (AppComponent) is not standalone.
+@Component({
+  selector: 'app-tic-tac-toe',
+  template: ''
+})
+class MockTicTacToeComponent {}
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        MockTicTacToeComponent
+      ],
+      // Because AppComponent is part of a module, it must be DECLARED.
+      // Its mocked child components must also be DECLARED.
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  // Your tests should now pass.
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'my-angular-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('my-angular-app');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.textContent).toContain('my-angular-app');
+  });
+});
